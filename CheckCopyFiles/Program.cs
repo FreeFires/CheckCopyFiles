8c34f3323fe72334ab3115a0a39f43b87066de1f
Andrii Simonenko <simonenkoff.a@gmail.com>
2021-02-16 12:50:13 +0200

    Перший коментар

---
+++ CheckCopyFiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace CheckCopyFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Run();
+        }
+
+        public static void Run()
+        {
+            string gotor = "D:\\Temp\\test1";
+
+            Console.WriteLine("Следим за директорией :" + gotor);
+            Console.ReadLine();
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = gotor;
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Filter = "*.*";
+
+        }
+    }
+}
